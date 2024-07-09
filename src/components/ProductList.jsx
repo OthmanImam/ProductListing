3c30c531cc90e571ecd8c507de9c6f2df73c6af7
Usman Adamu <imkargi@gmail.com>
2024-07-09 12:56:25 -0700

    Update ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,31 +3,31 @@ import ProductCard from './ProductCard';
 
 const products = [
   {
-    image: './public/img3.jpg',
+    image: 'img3.jpg',
     name: 'Mac Lipstick',
     price: 'N6000',
     rating: 0
   },
   {
-    image: './public/img4.jpg',
+    image: 'img4.jpg',
     name: 'Mac Lipstick',
     price: 'N5000',
     rating: 2
   },
   {
-    image: './public/img5.jpg',
+    image: 'img5.jpg',
     name: 'Mac Lipstick',
     price: 'N7000',
     rating: 3
   },
   {
-    image: './public/img6.jpg',
+    image: 'img6.jpg',
     name: 'Mac Lipstick',
     price: 'N2000',
     rating: 4
   },
   {
-    image: 'path_to_image5',
+    image: 'img7.jpg',
     name: 'Mac Lipstick',
     price: 'N15000',
     rating: 5
